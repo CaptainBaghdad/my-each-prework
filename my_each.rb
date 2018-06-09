068de8573c976fdf5aee7a71832b40e59de792e6
Jason Pitts <juniorjr083@gmail.com>
2018-06-09 02:38:28 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(obj) # put argument(s) here
   # code here
+  num  = 0  
+  while num < obj.length 
+  yield obj[num]
+  
+  
+end 
+  
+  return obj
 end
+
+my_each { |e| puts e}
